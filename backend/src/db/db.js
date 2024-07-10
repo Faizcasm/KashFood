308527a42180a88b4736306a97956dda95d658c2
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-07-10 06:47:05 -0700

    Update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const database = async()=>{
     try {
-        const connect = await mongoose.connect(`${process.env.url}/kashfood`)
+        const connect = await mongoose.connect(`mongodb+srv://faizanhameed690:faizan123@cluster0.hm1jull.mongodb.net/kashfood`)
         console.log("connected ",connect.connection.host);
     } catch (error) {
         console.log(error);
