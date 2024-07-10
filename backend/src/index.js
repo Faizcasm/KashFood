d27aade5379e01ac5471a6022f4d7f7e7799917a
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-07-09 23:53:50 -0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,9 +23,17 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['http://localhost:5173']);
+    res.set('Access-Control-Allow-Origin', ['http://localhost:5173']);
+    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+   res.set('Access-Control-Request-Headers', '*');
+      res.append('Access-Control-Allow-Origin', ['http://localhost:5173']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
+   res.append('Access-Control-Request-Headers', '*');
+        res.header('Access-Control-Allow-Origin', ['http://localhost:5173']);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
    res.header('Access-Control-Request-Headers', '*');
     next();
   });
