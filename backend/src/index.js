b374d78522e53068a8150403e870735ea5669ba5
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-07-09 21:33:22 -0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,13 @@ const options={
      methods: ["GET", "POST","PUT","DELETE"],
     optionsSuccessStatus: 200
 }
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://tiny-gelato-acf3fe.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(cors(options));
 app.use(express.static('public'))
