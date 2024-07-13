f7e38901fca28dbfa9065c87d316db43e1fbb2dd
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-07-12 23:38:22 -0700

    Update multer.js

--- backend/src/middlewares/multer.js
+++ backend/src/middlewares/multer.js
@@ -1,26 +1,14 @@
 import multer from "multer"
-import fs from 'fs'
-
-const filePath = 'public';
-
-try {
-fs.accessSync(filePath, fs.constants.F_OK);
 const storage = multer.diskStorage({
+    const filepath = './public',
     destination: function (req, file, cb) {
-      cb(null, filePath)
+      cb(null, filepath)
+    if(!filepath){
+        console.log("folder doesnot exists")
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
     }
   })
-} catch (err) {
-if (err.code === ‘ENOENT’) {
-console.error(‘File does not exist’);
-} else {
-// Handle other errors
-console.error(err);
-}
-}
-
   
  export const upload = multer({ storage })
