e285445c7d67ac4ea7b7b9661d5dffce89eaa392
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-07-12 21:34:06 -0700

    Update usercontroller.js

--- backend/src/controllers/usercontroller.js
+++ backend/src/controllers/usercontroller.js
@@ -35,12 +35,12 @@ const register =async(req,res)=>{
     console.log(avatarfilepath);
     if(!avatarfilepath){
         console.log("path missing");
-        return res.status(301)
+        return res.status(301).json({message:"No file path"})
     }
     const profile = await uploadOnCloudinary(avatarfilepath)
     if(!profile){
         console.log("Avatar required");
-        return res.status(301)
+        return res.status(301).json({message:"No profile photo"})
     }
     // const hashedpass = await bcrypt.hash(password,10)
     const cretedUser=await User.create({
